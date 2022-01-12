65885ebb96e2b52457aec27caaa141580441dd10
Daniil/Pecherin <daniel.pechyorin@mail.ru>
2022-01-12 18:42:35 +0300

    commit

---
+++ school/views.py
@@ -0,0 +1,17 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    obj_list = Student.objects.all()
+    context = {
+        'object_list': obj_list
+    }
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    ordering = 'group'
+
+    return render(request, template, context)
